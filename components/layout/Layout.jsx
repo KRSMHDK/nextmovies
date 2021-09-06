2a7fc576629ed41247433340e97d9b7613b6ec59
Kris <kris.mahadhika@gmail.com>
2021-09-06 11:41:40 +0800

    a

--- components/layout/Layout.jsx
+++ components/layout/Layout.jsx
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import MainHeader from './main-header';
+import MainHeader from './MainHeader';
 import Footer from './Footer';
 
 function Layout({ children }) {
