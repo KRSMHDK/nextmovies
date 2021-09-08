30646e1747ae2db2858c1a1ac51e727b15ecda4c
Kris <kris.mahadhika@gmail.com>
2021-09-08 10:30:56 +0800

    placeholder actor images

--- components/movie/Actors.jsx
+++ components/movie/Actors.jsx
@@ -11,7 +11,11 @@ function Actors({ actors }) {
             <img
               className="mx-auto w-28 rounded-xl"
               alt={actor.name}
-              src={`https://www.themoviedb.org/t/p/w138_and_h175_face${actor.profile_path}`}
+              src={
+                actor.profile_path === null
+                  ? 'https://upload.wikimedia.org/wikipedia/commons/6/65/No-Image-Placeholder.svg'
+                  : `https://www.themoviedb.org/t/p/w138_and_h175_face${actor.profile_path}`
+              }
             />
             <p className="pl-2 text-sm font-bold text-left">{actor.name}</p>
             <p className="pl-2 text-sm text-left ">{actor.character}</p>
