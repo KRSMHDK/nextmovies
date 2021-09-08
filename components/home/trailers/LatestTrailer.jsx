94ff32a5bf923c09d8f454736af0fdcc10b5f78c
Kris <kris.mahadhika@gmail.com>
2021-09-08 10:24:58 +0800

    changing media height

--- components/home/trailers/LatestTrailer.jsx
+++ components/home/trailers/LatestTrailer.jsx
@@ -20,22 +20,25 @@ function PopularTrailer({ latestTrailer }) {
         <div className="flex-none mt-5 bg-gray-900 bg-opacity-70 ">
           <p className="pt-5 pb-3 pl-5 text-2xl text-white">Latest Trailers</p>
           <ul className="flex overflow-x-scroll h-72 ">
-            {movies.map((movie) => (
-              <li key={movie.id} className="flex-none h-48 ml-5 w-72">
-                <Link href={`/movies/${movie.id}`}>
-                  <Image
-                    className="duration-300 rounded-lg hover:scale-105"
-                    src={`https://image.tmdb.org/t/p/w1280${movie.backdrop_path}`}
-                    alt=""
-                    height={192}
-                    width={288}
-                    placeholder="blur"
-                    blurDataURL="/images/blur.png"
-                  />
-                </Link>
-                <p className="flex-wrap text-center text-white align-center">{movie.title}</p>
-              </li>
-            ))}
+            {movies.map(
+              (movie) =>
+                movie.videos.results[0] !== undefined && (
+                  <li key={movie.id} className="flex-none h-48 ml-5 w-72">
+                    <Link href={`/movies/${movie.id}`}>
+                      <Image
+                        className="duration-300 rounded-lg hover:scale-105"
+                        src={`https://image.tmdb.org/t/p/w1280${movie.backdrop_path}`}
+                        alt=""
+                        height={192}
+                        width={288}
+                        placeholder="blur"
+                        blurDataURL="/images/blur.png"
+                      />
+                    </Link>
+                    <p className="flex-wrap text-center text-white align-center">{movie.title}</p>
+                  </li>
+                ),
+            )}
           </ul>
         </div>
       </div>
--- components/movie/Media.jsx
+++ components/movie/Media.jsx
@@ -5,16 +5,20 @@ function Media({ movieDetails }) {
   return (
     <div className="h-auto max-w-screen-xl pt-5 pl-5 mx-auto mt-2 ">
       <p className="text-lg font-bold">Media</p>
-      <ul className="flex flex-shrink-0 pb-5 mt-3 overflow-x-scroll h-72 ">
-        {movieDetails.images.backdrops.map((movie) => (
-          <li key={movie.file_path} className="flex-shrink-0 mr-5 shadow-lg rounded-xl ">
-            <img
-              className="w-full h-full mx-auto rounded-xl"
-              alt=""
-              src={`https://www.themoviedb.org/t/p/w533_and_h300_bestv2${movie.file_path}`}
-            />
-          </li>
-        ))}
+      <ul className="flex pt-3 pb-5 overflow-x-scroll ">
+        {movieDetails.images.backdrops.length === 0 ? (
+          <div>No Backdrops Found </div>
+        ) : (
+          movieDetails.images.backdrops.map((movie) => (
+            <li key={movie.file_path} className="flex-shrink-0 mr-5 shadow-lg rounded-xl ">
+              <img
+                className="w-full h-full mx-auto rounded-xl"
+                alt=""
+                src={`https://www.themoviedb.org/t/p/w533_and_h300_bestv2${movie.file_path}`}
+              />
+            </li>
+          ))
+        )}
       </ul>
     </div>
   );
