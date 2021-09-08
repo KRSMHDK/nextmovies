1610de6ff57b9e802f09869f7e87ce5ed67ee3dd
Kris <kris.mahadhika@gmail.com>
2021-09-08 15:21:07 +0800

    changing search layout

--- components/search/SearchResults.jsx
+++ components/search/SearchResults.jsx
@@ -5,33 +5,36 @@ import Link from 'next/link';
 
 function SearchResults({ results }) {
   return (
-    <div className="flex">
+    <div className="flex-row">
       <div>
-        <div className="ml-40 mt-20 w-14 ">
-          <p className="w-52 border-2 bg-blue-500 rounded-t-lg text-white py-5 text-center text-xl">
+        <div className="pt-5">
+          <p className="h-auto px-4 py-2 text-left text-white bg-blue-500 rounded-t-lg text-md md:w-52">
             Search Results
           </p>
-          <p className="w-52 h-20 text-left px-5 py-5 rounded-b-lg text-lg border-2 bg-gray-200">
-            Movies -{results.data.total_results}
+          <p className="px-4 py-2 text-sm text-left border-b-2 md:w-52">
+            Found {results.data.total_results} Movies
           </p>
         </div>
       </div>
       <div>
-        <ul className="mt-20 ">
+        <ul className="pr-20 mt-20 ">
           {results.data.results.map((movie) => (
-            <li className="shadow-md ml-56 mr-96 border-2 rounded-lg mb-5 flex " key={movie.id}>
+            <li
+              className="flex min-w-0 min-h-0 mb-5 border-2 rounded-lg shadow-md h-4/6"
+              key={movie.id}
+            >
               <Link href={`/movies/${movie.id}`}>
                 <img
-                  className="h-40 w-28 flex-none  "
+                  className="flex-none h-40 w-28 "
                   src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
                   alt=""
                 />
               </Link>
-              <div className="ml-4">
-                <p className=" text-2xl bold ">{movie.title}</p>
+              <div className="ml-4 ">
+                <p className="text-2xl bold">{movie.title}</p>
 
                 <p className="mb-2">{movie.release_date}</p>
-                <p className="mb-2 overflow-hidden mr-12">{movie.overview}</p>
+                <p className="w-full max-h-full mb-2 ">{movie.overview}</p>
               </div>
             </li>
           ))}
