63b9cf26a4cb57f12af622c2c463ca82687b2d33
Kris <kris.mahadhika@gmail.com>
2021-09-08 14:23:13 +0800

    added percentage bar

--- components/home/popular/PopularMovieList.jsx
+++ components/home/popular/PopularMovieList.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import dateformat from 'dateformat';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 
 function PopularMovieList({ movies }) {
   return (
@@ -10,18 +12,42 @@ function PopularMovieList({ movies }) {
       <ul className="flex overflow-x-scroll ">
         {movies.results.map((movie) => (
           <li key={movie.id} className="flex-none ml-2 ">
-            <Link href={`/movies/${movie.id}`}>
-              <Image
-                className="border rounded-lg cursor-pointer"
-                src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-                alt=""
-                width={150}
-                height={224}
-                placeholder="blur"
-                blurDataURL="/images/blur.png"
+            <div className="relative">
+              <Link className="" href={`/movies/${movie.id}`}>
+                <Image
+                  className="relative border rounded-lg cursor-pointer "
+                  src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                  alt=""
+                  width={150}
+                  height={224}
+                  placeholder="blur"
+                  blurDataURL="/images/blur.png"
+                />
+              </Link>
+              <CircularProgressbar
+                className="absolute h-11 -bottom-4 -left-14"
+                background
+                value={movie.vote_average * 10}
+                text={`${movie.vote_average * 10}%`}
+                styles={{
+                  path: {
+                    stroke: '#0fe757be',
+                    strokeLinecap: 'butt',
+                  },
+                  trail: {
+                    stroke: '#ffffff',
+                  },
+                  text: {
+                    fill: '#fff',
+                    fontSize: '26px',
+                  },
+                  background: {
+                    fill: '#080303',
+                  },
+                }}
               />
-            </Link>
-            <div className="w-40 overflow-wrap">
+            </div>
+            <div className="w-40 pt-5 overflow-wrap">
               <p className="pl-2 text-sm font-bold text-black">{movie.title}</p>
               <p className="pb-4 pl-2 text-sm font-light text-gray-600">
                 {dateformat(movie.release_date, 'mediumDate')}
