a803a6a2520b7e4d46a3d684a55bec05aae54c17
Kris <kris.mahadhika@gmail.com>
2021-09-08 13:34:31 +0800

    add padding left

--- components/movie/Actors.jsx
+++ components/movie/Actors.jsx
@@ -3,14 +3,11 @@ import React from 'react';
 
 function Actors({ actors }) {
   return (
-    <div className="h-auto max-w-screen-xl pt-5 mx-auto mt-2 ">
+    <div className="h-auto max-w-screen-xl pt-5 pl-5 mx-auto mt-2 ">
       <p className="text-lg font-bold">Top Billed Cast </p>
       <ul className="flex flex-shrink-0 h-auto pb-5 mt-3 overflow-x-scroll ">
         {actors.cast.map((actor) => (
-          <li
-            key={actor.name}
-            className="flex-shrink-0 h-auto pb-2 mr-5 shadow-lg  w-28 rounded-xl"
-          >
+          <li key={actor.name} className="flex-shrink-0 h-auto pb-2 mr-5 shadow-lg w-28 rounded-xl">
             <img
               className="mx-auto w-28 rounded-xl"
               alt={actor.name}
