9d2424084f4be2ba86b88968368daca144b1d1be
Kris <kris.mahadhika@gmail.com>
2021-09-10 13:26:48 +0800

    movie media backdrops/poster fixed

--- components/movie/Media.jsx
+++ components/movie/Media.jsx
@@ -1,24 +1,93 @@
 /* eslint-disable react/prop-types */
-import React from 'react';
+import React, { Fragment, useState } from 'react';
 import Image from 'next/image';
+import { Tab } from '@headlessui/react';
 
 function Media({ movieDetails }) {
+  const backdrops = {
+    type: 'backdrops',
+    path: 'file_path',
+    width: 533,
+    height: 300,
+    img_path: 'https://www.themoviedb.org/t/p/w533_and_h300_bestv2',
+  };
+  const posters = {
+    type: 'posters',
+    path: 'file_path',
+    width: 200,
+    height: 300,
+    img_path: 'https://www.themoviedb.org/t/p/w220_and_h330_face',
+  };
+
+  const [selectedType, setSelectedType] = useState(backdrops);
+
   return (
     <div className="h-auto max-w-screen-xl pt-5 pl-5 mx-auto mt-2 ">
-      <p className="text-lg font-bold">Media</p>
+      <Tab.Group>
+        <Tab.List className="flex gap-2 ">
+          <h1 className="mr-10 text-xl font-bold border-b-4 border-transparent">Media</h1>
+          {/* <Tab as={Fragment}>
+            {({ selected }) => (
+              <button
+                type="button"
+                className={selected ? 'underline font-bold ' : 'text-black'}
+                value="Movie"
+                onClick={() => setSelectedType(backdrops)}
+              >
+                Movie
+              </button>
+            )}
+          </Tab> */}
+          <Tab as={Fragment}>
+            {({ selected }) => (
+              <button
+                type="button"
+                className={
+                  selected
+                    ? 'border-b-4 border-black font-bold '
+                    : 'text-black border-b-4 border-transparent font-bold'
+                }
+                value="Backdrops"
+                onClick={() => setSelectedType(backdrops)}
+              >
+                Backdrops
+              </button>
+            )}
+          </Tab>
+          <Tab as={Fragment}>
+            {({ selected }) => (
+              <button
+                type="button"
+                className={
+                  selected
+                    ? 'border-b-4 border-black font-bold '
+                    : 'text-black border-b-4 border-transparent font-bold'
+                }
+                value="Posters"
+                onClick={() => setSelectedType(posters)}
+              >
+                Posters
+              </button>
+            )}
+          </Tab>
+        </Tab.List>
+      </Tab.Group>
+
       <ul className="flex mt-3 overflow-x-scroll rounded-xl">
-        {movieDetails.images.backdrops.length === 0 ? (
+        {movieDetails.images[selectedType.type].length === 0 ? (
           <div>No Backdrops Found </div>
         ) : (
-          movieDetails.images.backdrops.map((movie) => (
-            <li key={movie.file_path} className="flex-shrink-0 ">
+          movieDetails.images[selectedType.type].map((movie) => (
+            <li key={movie[selectedType.path]} className="flex-shrink-0 ">
               <Image
                 className="mx-auto "
                 placeholder="blur"
                 blurDataURL="/images/blur.png"
                 width={533}
                 height={300}
-                src={`https://www.themoviedb.org/t/p/w533_and_h300_bestv2${movie.file_path}`}
+                src={`https://www.themoviedb.org/t/p/w533_and_h300_bestv2${
+                  movie[selectedType.path]
+                }`}
               />
             </li>
           ))
